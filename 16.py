dc27ab19e4b2698eec031abb4df4d362c29723c9
charosqadirova13-hub <charosqadirova13@gmail.com>
2026-04-14 16:16:03 +0500

    Add factorial and digit sum calculations; implement reverse number function

--- 16.py
+++ 16.py
@@ -1,3 +1,10 @@
 import math
 n = int(input())
 print(math.factorial(n))
+
+# 2 
+n = int(input())
+s = 1
+for son in range(1, n+1):
+    s *= son
+print(s)
---
+++ 19.py
@@ -0,0 +1,5 @@
+n = input()
+sum = 0
+for raqam in n:
+    sum += int(raqam)
+ print(sum)   
